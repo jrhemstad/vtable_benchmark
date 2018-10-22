20686050b9260d68e1f1e7f29066e532d4cab0c7
Jake Hemstad <jhemstad@nvidia.com>
2018-10-21 18:12:40 -0700

    Compiling.

--- main.cpp
+++ main.cpp
@@ -1,33 +1,38 @@
-
+#include <vector>
+#include <iostream>
+#include <typeinfo>
 
 enum types
 {
   CHAR,
   INT,
   FLOAT,
-  DOUBLE,
-}
+  DOUBLE
+};
 
 struct column
 {
   void * data;
   types t;
+  int size;
 };
 
-template <typname T>
+template <typename T>
 column make_column(std::vector<T> v)
 {
   column the_column;
 
   the_column.data = v.data();
+  the_column.size = v.size();
 
-  if(std::is_same<T, char>::value) v.t = CHAR;
-  else if(std::is_same<T, int>::value) v.t = INT;
-  else if(std::is_same<T, float>::value) v.t = FLOAT;
-  else if(std::is_same<T, double>::value) v.t = DOUBLE;
+  if(std::is_same<T, char>::value) the_column.t = CHAR;
+  else if(std::is_same<T, int>::value) the_column.t = INT;
+  else if(std::is_same<T, float>::value) the_column.t = FLOAT;
+  else if(std::is_same<T, double>::value) the_column.t = DOUBLE;
   else
   {
     std::cout << "Invalid vector type.\n";
+    return column{nullptr,CHAR};
   }
 
   return the_column;
@@ -39,60 +44,96 @@ void add_column_elements(column const& l_column, const int l_index,
   switch(l_column.t)
   {
     case CHAR:
-      using col_type = char;
-      col_type l_col = static_cast<col_type>(l_column.data);
-      col_type r_col = static_cast<col_type>(r_column.data);
-      l_col[l_index] += r_col[r_index];
-      break;
+      {
+        using col_type = char;
+        col_type * l_col = static_cast<col_type*>(l_column.data);
+        col_type * r_col = static_cast<col_type*>(r_column.data);
+        l_col[l_index] += r_col[r_index];
+        break;
+      }
     case INT:
-      using col_type = int;
-      col_type l_col = static_cast<col_type>(l_column.data);
-      col_type r_col = static_cast<col_type>(r_column.data);
-      l_col[l_index] += r_col[r_index];
-      break;
+      {
+        using col_type = int;
+        col_type * l_col = static_cast<col_type*>(l_column.data);
+        col_type * r_col = static_cast<col_type*>(r_column.data);
+        l_col[l_index] += r_col[r_index];
+        break;
+      }
     case FLOAT:
-      using col_type = float;
-      col_type l_col = static_cast<col_type>(l_column.data);
-      col_type r_col = static_cast<col_type>(r_column.data);
-      l_col[l_index] += r_col[r_index];
-      break;
+      {
+        using col_type = float;
+        col_type * l_col = static_cast<col_type*>(l_column.data);
+        col_type * r_col = static_cast<col_type*>(r_column.data);
+        l_col[l_index] += r_col[r_index];
+        break;
+      }
     case DOUBLE:
-      using col_type = double;
-      col_type l_col = static_cast<col_type>(l_column.data);
-      col_type r_col = static_cast<col_type>(r_column.data);
-      l_col[l_index] += r_col[r_index];
-      break;
+      {
+        using col_type = double;
+        col_type * l_col = static_cast<col_type*>(l_column.data);
+        col_type * r_col = static_cast<col_type*>(r_column.data);
+        l_col[l_index] += r_col[r_index];
+        break;
+      }
     default:
+      return;
   }
 }
 
 struct BaseColumn
 {
+
   virtual void add_element(BaseColumn const& other_column, const int my_index, const int other_index ) = 0;
+
+protected:
+  BaseColumn(column the_column) : base_data{the_column.data}, size{the_column.size}
+  {}
+  void * base_data;
+  int size;
 };
 
 template <typename T>
 struct TypedColumn : BaseColumn
 {
 
-  TypeColumn(std::vector<T> v) : data{v.data()}, size(v.size())
-  { }
-
-  TypedColumn(T * _data, int _size) : data{_data}, size{_size}
-  { }
+  TypedColumn(column the_column) : BaseColumn{the_column}, data{static_cast<T*>(base_data)}
+  { 
+  }
 
   virtual void add_element(BaseColumn const& other_column, const int my_index, const int other_index ) override
   {
-    data[my_index] += outher_column.data[other_index];
+    // DANGER: This assumes other_column is a TypedColumn<T> with the same T...
+    // Is there some way to guarantee that they are the same?
+    // Solution 1: Check that the enum types are equal
+    // Solution 2: Use dynamic cast and check for nullptr/thrown exception
+    // Check the overhead of this...
+    try {
+      TypedColumn<T> const& actual_column = dynamic_cast< TypedColumn<T> const&>(other_column);
+      std::cout << "Adding " << data[my_index] << " and " << actual_column.data[other_index] << std::endl;
+      data[my_index] += actual_column.data[other_index];
+    } 
+    catch(const std::bad_cast& e) {
+      printf("Tried to add two columns of different types.\n");
+    }
   }
 
 private:
   T * data;
-  int size;
 };
 
 int main()
 {
-  std::vector<int> left(100);
-  std::vecotr<int> right(100);
+  std::vector<int> left(100,1);
+  std::vector<int> right(100,2);
+
+  column left_column = make_column(left);
+  column right_column = make_column(right);
+
+  BaseColumn * left_base = new TypedColumn<int>(left_column);
+  BaseColumn * right_base = new TypedColumn<int>(right_column);
+
+  left_base->add_element(*right_base, 0,0);
+
+  std::cout << left[0] << std::endl;
+  std::cout << right[0] << std::endl;
 }
